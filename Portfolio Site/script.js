620cc94bf7219c680e565899d77466ce0dd7bccd
Eric Andrade <eandrade21@gmail.com>
2017-04-24 19:02:01 -0700

    changes to script file and to thumbnail classes

--- Portfolio Site/script.js
+++ Portfolio Site/script.js
@@ -1,4 +1,11 @@
-$('.thumbnail').click(function (evt) {
-    // var objClicked = evt;
-    $('.thumbnail').remove();
+$('.thumbnail').click(function() {
+    var objClicked = $(this);
+    console.log(objClicked);
+    $('.thumbnail').hide();
+
+    if(objClicked.hasClass('project1') === true){
+        console.log('This is project 1');
+    } else if(objClicked.hasClass('project2') === true){
+        console.log('This is project 2');
+    }
 });
